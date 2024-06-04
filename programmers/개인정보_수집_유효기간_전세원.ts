be602b71b937c1f98312094397773ac00fc8a0f5
sewonjun <thornapple74@gmail.com>
2024-06-04 11:01:15 +0900

    solved programmers 개인정보 수집 유효기간

--- programmers/개인정보_수집_유효기간_전세원.ts
+++ programmers/개인정보_수집_유효기간_전세원.ts
@@ -1,65 +1,75 @@
-//가장 먼저 약관 종류에 따라, 달을 더하고, 기존 달 + 유효 기간/12 로 년도를 바꿀지 판별한다.
-//후에 기존 달 + 유효 기간%12로 달을 판별한다.
-//달을 더 한후, -1일을 한다.
-
-//엣지 케이스
-//1일 수집일자이면, 28일까지만 수집이 가능하다. => 그러면서 달에서 -1 해야 한다.
-//1월 1일인 경우 전년도 12월 28일까지 수집이 가능.
-
-function isExpired(date, term) {
-  let [year, month, day] = date.split(".").map((el) => parseInt(el, 10));
-  const termSumedMonth = month + term;
-
-  if (termSumedMonth / 12 > 1) {
-    const yearUpdate = Math.floor(termSumedMonth / 12);
-
-    year += yearUpdate;
-  }
-  //12 % 12 일 경우, 년도 업데이트 - 1
-  const monthUpdate = termSumedMonth % 12;
-
-  if (monthUpdate === 0) {
-    year--;
-    month += term % 12;
-  } else {
-    month = monthUpdate;
-  }
-
-  return [year, month, day];
-}
-
-function solution(today, terms, privacies) {
-  const result = [];
-  const termsObj = terms.reduce((acc, cur) => {
+function solution(today: string, terms: string[], privacies: string[]) {
+  const answer: number[] = [];
+  const splittedTerms = terms.reduce((acc: { [key: string]: number }, cur) => {
     const [key, value] = cur.split(" ");
-    acc[key] = value;
+    acc[key] = parseInt(value, 10);
 
     return acc;
   }, {});
-  const [curYear, curMonth, curDay] = today
-    .split(".")
-    .map((el) => parseInt(el, 10));
 
-  privacies.forEach((privacy, index) => {
-    const [signupDate, term] = privacy.split(" ");
-    const termMonth = termsObj[term];
-    const [dueYear, dueMonth, dueDay] = isExpired(
-      signupDate,
-      Number(termMonth)
-    );
+  const privacyEndDate = privacies.map((privacy) => {
+    const [date, term] = privacy.split(" ");
+    const [year, month, day] = date.split(".").map((el) => parseInt(el, 10));
+    let endYear, endMonth;
+    const termMonth = splittedTerms[term];
 
-    if (dueYear < curYear) {
-      result.push(index + 1);
-    } else if (dueYear === curYear && dueMonth < curMonth) {
-      result.push(index + 1);
-    } else if (
-      dueYear === curYear &&
-      dueMonth === curMonth &&
-      dueDay <= curDay
+    if (
+      Math.floor((month + termMonth) / 12) > 0 &&
+      (month + termMonth) % 12 === 0
     ) {
-      result.push(index + 1);
+      endYear = year + Math.floor((month + termMonth) / 12) - 1;
+    } else if (Math.floor(month + termMonth / 12) > 0) {
+      endYear = year + Math.floor((month + termMonth) / 12);
+    } else {
+      endYear = year;
+    }
+
+    endMonth = (month + termMonth) % 12 !== 0 ? (month + termMonth) % 12 : 12;
+
+    return [endYear, endMonth, day];
+  });
+
+  const lastPrivacyDates = privacyEndDate.map((privacy) => {
+    const [year, month, day] = privacy;
+    let lastPrivacyYear, lastPrivacyMonth, lastPrivacyDay;
+
+    if (day === 1) {
+      lastPrivacyDay = 28;
+    } else {
+      lastPrivacyDay = day - 1;
+
+      return [year, month, lastPrivacyDay];
+    }
+
+    if (month === 1) {
+      lastPrivacyMonth = 12;
+      lastPrivacyYear = year - 1;
+
+      return [lastPrivacyYear, lastPrivacyMonth, lastPrivacyDay];
+    } else {
+      lastPrivacyMonth = month - 1;
+
+      return [year, lastPrivacyMonth, lastPrivacyDay];
     }
   });
 
-  return result;
+  const [todayYear, todayMonth, todayDay] = today
+    .split(".")
+    .map((el) => parseInt(el, 10));
+
+  lastPrivacyDates.forEach((el, index) => {
+    const [year, month, day] = el;
+
+    if (todayYear < year) return;
+
+    if (todayYear > year) return answer.push(index + 1);
+
+    if (todayMonth < month) return;
+
+    if (todayMonth > month) return answer.push(index + 1);
+
+    if (todayDay > day) answer.push(index + 1);
+  });
+
+  return answer;
 }
