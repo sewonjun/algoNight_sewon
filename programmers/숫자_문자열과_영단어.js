4d4026e36160e42452a738348bca16e9b2a3decf
sewonjun <thornapple74@gmail.com>
2024-06-19 23:16:12 +0900

    solved 숫자 문자열과 영단어

---
+++ programmers/숫자_문자열과_영단어.js
@@ -0,0 +1,32 @@
+function solution(s) {
+  const nums = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  let answer = "";
+  let stack = "";
+
+  for (el of s) {
+    if (!isNaN(el)) {
+      answer += el;
+    } else {
+      stack += el;
+      const isNum = nums.hasOwnProperty(stack);
+      if (isNum) {
+        answer += nums[stack];
+        stack = "";
+      }
+    }
+  }
+
+  return parseInt(answer, 10);
+}
