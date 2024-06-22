b254c60986e9e6ae2041db6f4567f88c5378e101
sewonjun <thornapple74@gmail.com>
2024-06-22 21:58:07 +0900

    solved invert binary tree

---
+++ leetcode/226_Invert_Binary_Tree_전세원.ts
@@ -0,0 +1,30 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (root === null) return root;
+
+  if (root?.left === null && root?.right === null) {
+    return root;
+  }
+
+  const temp = root?.left;
+
+  root.left = root?.right;
+
+  root.right = temp;
+
+  if (root?.left) invertTree(root.left);
+
+  if (root?.right) invertTree(root.right);
+
+  return root;
+}
